a174695c8fa420975d28c3ff4a5dd6088bd251a3
João Victor <joaovictormdasilva676@gmail.com>
2024-09-08 17:49:22 -0300

    :sparkles: feat: Buscando e atualizando objetos

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ const livros = [
     }
 ]
 
+function bucarPorId(id) {
+    return livros.findIndex(livro => livro.id === Number(id));
+};
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
@@ -22,10 +26,20 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+    const index = bucarPorId(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
 app.post("/livros", (req, res) => {
     livros.push(req.body);
-    console.log(req);
     res.status(201).send("Livro Criado com sucesso!");
-})
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = bucarPorId(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros[index]);
+});
 
 export default app;
