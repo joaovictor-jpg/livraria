fe6877d99a8fb32baf027332472dc88af4b1fabc
João Victor <joaovictormdasilva676@gmail.com>
2024-09-08 17:30:22 -0300

    :sparkles: feat: Criando registro com post

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+const livros = [
+    {
+        id: 1,
+        titulo: "O Senhor dos Anéis"
+    },
+    {
+        id: 1,
+        titulo: "O Hobbit"
+    }
+]
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    console.log(req);
+    res.status(201).send("Livro Criado com sucesso!");
+})
 
 export default app;
