e661381a7ce0dfe229c30b10f90ec8680fbc9608
João Victor <joaovictormdasilva676@gmail.com>
2024-09-09 17:42:48 -0300

    :sparkles: feat: Criando o endpoint para deletar livro

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -18,7 +18,7 @@ class LivroController {
         } catch (error) {
             res.status(500).json({ message: `${error.message} - Falha na requisição do livro` })
         }
-    }
+    };
 
     static async cadastrarLivro(req, res) {
         try {
@@ -27,7 +27,7 @@ class LivroController {
         } catch (error) {
             res.status(500).json({ mensagem: `${error.mensagem} - falha ao cadastrar livro` });
         }
-    }
+    };
 
     static async atualizar(req, res) {
         try {
@@ -37,7 +37,17 @@ class LivroController {
         } catch (error) {
             res.status(500).json({ message: `${error.message} - Falha ao atualizar livro` })
         }
-    }
+    };
+
+    static async deleterLivro(req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(201).json({ message: "Livro deletado com sucesso" });
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - Falha ao tentar deletar livro` })
+        }
+    };
 };
 
 export default LivroController;
--- src/routes/livrosRouters.js
+++ src/routes/livrosRouters.js
@@ -4,8 +4,9 @@ import LivroController from "../controllers/livroController.js";
 const routes = express.Router();
 
 routes.get("/livros", LivroController.listarLivros);
-routes.get("/livro/:id", LivroController.listaLivroPorId);
-routes.post("/livro", LivroController.cadastrarLivro);
-routes.put("livro/:id", LivroController.atualizar);
+routes.get("/livros/:id", LivroController.listaLivroPorId);
+routes.post("/livros", LivroController.cadastrarLivro);
+routes.put("/livros/:id", LivroController.atualizar);
+routes.delete("/livros/:id", LivroController.deleterLivro);
 
 export default routes;
